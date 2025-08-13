e7a1a2fdead67a63e12a44366e190ba4b7927c11
Allan Barbosa <allan.barbosa@fiscaltech.com.br>
2025-08-13 19:07:45 +0000

    Add WiringPi.

--- embedded-system/src/main.cpp
+++ embedded-system/src/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <wiringPi.h>
+
+#define	LED	0
 
 int main() {
   std::cout << "====================================" << std::endl;
   std::cout << "Welcome to Cardiac Monitoring System" << std::endl;
   std::cout << "====================================" << std::endl;
+
+  std::cout << "Raspberry Pi blink" << std::endl;
+  wiringPiSetup();
+  pinMode(LED, OUTPUT);
+
+  while (true) {
+      digitalWrite (LED, HIGH);
+    delay (500);
+    digitalWrite (LED, LOW);
+    delay (500);
+  }
+  
   return 0;
 }
