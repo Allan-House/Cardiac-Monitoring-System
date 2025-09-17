f83f6514732868d0f31c728b4f6a162b01b82411
Allan-House <allanronaldo313@gmail.com>
2025-09-17 01:21:42 +0000

    lazy commit.

--- src/main.cpp
+++ src/main.cpp
@@ -20,29 +20,17 @@
   #include "file_data.h"
 #endif
 
-std::shared_ptr<DataSource> createDataSource(int argc, char** argv) {
-#ifdef USE_HARDWARE_SOURCE
-  std::cout << "Using hardware data source (Raspberry Pi)" << std::endl;
-  auto ads1115 = std::make_shared<ADS1115>();
-  return std::make_shared<SensorData>(ads1115);
-  
-#elif defined(USE_FILE_SOURCE)
-  std::string filename = "/workspaces/Cardiac-Monitoring-System/data/ecg_samples.bin"; // Default file
-  
-  if (argc > 1) {
-    filename = argv[1];
-    std::cout << "Using file data source: " << filename << std::endl;
-  } else {
-    std::cout << "Using default file data source: " << filename << std::endl;
-  }
-  
-  return std::make_shared<FileData>(filename);
-  
-#else
-  #error "No data source defined. Check CMake configuration."
-#endif
-}
+std::shared_ptr<DataSource> createDataSource(int argc, char** argv);
 
+/**
+ * 250 SPS x 300s -> 75000 samples
+ * 475 SPS x 300s -> 142500 samples
+*/
+
+/* TODO (allan): Escolhas a serem feitas:
+- Instanciar os objetos estaticamente ou dinamicamente?
+- Argumentos possíveis.
+*/
 int main(int argc, char** argv) {
   cardiac_logger::init("cardiac_system.log", cardiac_logger::Level::kDebug);
 
@@ -82,4 +70,27 @@ int main(int argc, char** argv) {
   }
 
   return 0;
+}
+
+std::shared_ptr<DataSource> createDataSource(int argc, char** argv) {
+#ifdef USE_HARDWARE_SOURCE
+  std::cout << "Using hardware data source (Raspberry Pi)" << std::endl;
+  auto ads1115 = std::make_shared<ADS1115>();
+  return std::make_shared<SensorData>(ads1115);
+  
+#elif defined(USE_FILE_SOURCE)
+  std::string filename = "/workspaces/Cardiac-Monitoring-System/data/ecg_samples.bin"; // Default file
+  
+  if (argc > 1) {
+    filename = argv[1];
+    std::cout << "Using file data source: " << filename << std::endl;
+  } else {
+    std::cout << "Using default file data source: " << filename << std::endl;
+  }
+  
+  return std::make_shared<FileData>(filename);
+  
+#else
+  #error "No data source defined. Check CMake configuration."
+#endif
 }
